af35b3caf3791d2d074f38cd9a49894a1ff3b739
靳兆鲁 <1756404649@qq.com>
2019-04-23 03:20:34 +0800

    修复 播放器菜单弹出时导航栏也会弹出的问题

--- nvplayer/src/main/java/com/nesp/nvplayer/dialog/RightSlideMenuDialog.java
+++ nvplayer/src/main/java/com/nesp/nvplayer/dialog/RightSlideMenuDialog.java
@@ -3,6 +3,7 @@ package com.nesp.nvplayer.dialog;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -140,4 +141,25 @@ public class RightSlideMenuDialog extends Dialog {
 
     }
 
+
+    private void fullScreenImmersive(View view){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
+            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            view.setSystemUiVisibility(uiOptions);
+        }
+    }
+
+    @Override
+    public void show() {
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        super.show();
+        fullScreenImmersive(window.getDecorView());
+        window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+    }
 }
