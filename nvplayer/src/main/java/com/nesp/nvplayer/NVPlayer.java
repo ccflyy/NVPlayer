e00ab37adcf3068a1626a29d532f977c1a3e2b04
jinzhaolu <1756404649@qq.com>
2019-05-03 20:36:01 +0800

    Update Copyright

--- nvplayer/src/main/java/com/nesp/nvplayer/NVPlayer.java
+++ nvplayer/src/main/java/com/nesp/nvplayer/NVPlayer.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *   Copyright (c) 2019  NESP Technology Corporation. All rights reserved.
+ *
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms and conditions of the GNU General Public License,
+ *   version 2, as published by the Free Software Foundation.
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License.See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *   If you have any questions or if you find a bug,
+ *   please contact the author by email or ask for Issues.
+ *
+ *   Author:JinZhaolu <1756404649@qq.com>
+ */
+
 package com.nesp.nvplayer;
 
 import android.annotation.SuppressLint;
@@ -834,7 +852,7 @@ public class NVPlayer extends NormalGSYVideoPlayer {
             localLayoutParams.gravity = Gravity.TOP | Gravity.RIGHT;
             localLayoutParams.width = getWidth();
             localLayoutParams.height = getHeight();
-            int location[] = new int[2];
+            int[] location = new int[2];
             getLocationOnScreen(location);
             localLayoutParams.x = location[0];
             localLayoutParams.y = location[1];
@@ -881,7 +899,7 @@ public class NVPlayer extends NormalGSYVideoPlayer {
             localLayoutParams.gravity = Gravity.TOP | Gravity.LEFT;
             localLayoutParams.width = getWidth();
             localLayoutParams.height = getHeight();
-            int location[] = new int[2];
+            int[] location = new int[2];
             getLocationOnScreen(location);
             localLayoutParams.x = location[0];
             localLayoutParams.y = location[1];
